d62065545818e604af203172371f3f907db8bdf8
Lord of Harem <lords.of.harem@gmail.com>
2018-04-03 15:07:14 +0200

    Ajout d'un cron pour le lancement automatique de la tâche

--- src/index.js
+++ src/index.js
@@ -2,18 +2,26 @@ import {fetchAllStats, fields} from './reader'
 import {saveStats} from './db'
 import {EventEmitter} from 'events';
 import cliProgress from 'cli-progress';
+import {CronJob} from 'cron';
 
-const bar = new cliProgress.Bar({}, cliProgress.Presets.legacy);
-const e = new EventEmitter();
+new CronJob('0 15 4 * * *',
+    () => {
+        const bar = new cliProgress.Bar({}, cliProgress.Presets.legacy);
+        const e = new EventEmitter();
 
-e
-    .on('start', nbPages => bar.start(nbPages * fields.length, 0))
-    .on('page', () => bar.increment())
-    .on('ready', () => fetchAllStats(e))
-    .on('end', () => {
-        bar.stop();
-        console.log('finish');
-    })
+        e
+            .on('start', nbPages => bar.start(nbPages * fields.length, 0))
+            .on('page', () => bar.increment())
+            .on('ready', () => fetchAllStats(e))
+            .on('end', () => {
+                bar.stop();
+                console.log('finish');
+            })
+        ;
+
+        saveStats(e);
+    },
+    null,
+    true,
+    'Europe/Paris')
 ;
-
-saveStats(e);
